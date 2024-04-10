1fae2d2378028138c4e789fe3da57df65e38d65f
Halan Caio <hallankayo20@gmail.com>
2024-04-10 17:06:15 -0300

    gitignore config

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,4 @@
 import './App.css'
-<<<<<<< HEAD
-import Cadastro from './pages/cadastro/Cadastro'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-function App() {
-
-
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/cadastro" element = {<Cadastro/>}/>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
-=======
 import SingUp from "./pages/SingUp";
 import Home from "./pages/Home"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -34,7 +17,6 @@ function App() {
     </BrowserRouter>
   </>
   );
->>>>>>> 41c900c353ee6437314a07e0f956d689500e99ca
 }
 
 export default App
