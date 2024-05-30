ba3b2661c96d3aa15bcd6a61775373ae380e3c3a
Halan-Caio <halan.nascimento@sinerji.com.br>
2024-05-30 20:46:02 -0300

    feat: remover treino de usuário

--- Backend/routes/TreinoAluno.py
+++ Backend/routes/TreinoAluno.py
@@ -17,27 +17,17 @@ app = Flask(__name__)
 
 treinoUsuario_bp = Blueprint('treinoUsuario', __name__)
 
-# Função para verificar se o usuário é um aluno
-# def is_aluno(user_id):
-#     try:
-        
-#         usuario = User.getById(user_id)
-#         return usuario.tipoUsuario.descricao == 'Aluno'
-#     except User.DoesNotExist:
-#         return False
-
 @treinoUsuario_bp.route('/ExerciseUser', methods=['POST'])
 def adicionar_usuario_treino():
     data = request.get_json()
     try:
-        aluno = Usuario.select().where(Usuario.id == data["alunoId"]).first()
+        Usuario.select().where(Usuario.id == data["alunoId"]).first()
     except User.DoesNotExist:
         return jsonify({'error': 'Aluno não encontrado'}), 404
     try:
-        treinoReferente = Treinos.getExercicioById(data["exercicioId"])
+        Treinos.getExercicioById(data["exercicioId"])
     except Exception:
-        return jsonify({'error': str("quebrou")}), 400
-    print(jsonify(treinoReferente))
+        return jsonify({'error': str("Exercício não encontrado")}), 400
 
     usuario_treino = UsuarioTreino(
         usuarioId=data["alunoId"],
@@ -50,3 +40,21 @@ def adicionar_usuario_treino():
         return jsonify({'message': 'Treino adicionado com sucesso'}), 201
     else:
         return jsonify({'error': 'Somente usuários do tipo "Aluno" podem ser vinculados a treinos'}), 400
+    
+@treinoUsuario_bp.route('/ExerciseUser', methods=['DELETE'])
+def excluir_treino_aluno():
+    data = request.get_json()
+    try: 
+        Usuario.select().where(Usuario.id == data["alunoId"]).first()
+    except User.DoesNotExist:
+        return jsonify({'error': 'Aluno não encontrado'}), 404
+    try:
+        Treinos.getExercicioById(data["exercicioId"])
+    except Exception:
+        return jsonify({'error': str("Exercício não encontrado")}), 400
+    
+    treino_usuario = UsuarioTreino.delete().where(
+        UsuarioTreino.usuarioId_id == data["alunoId"] and UsuarioTreino.usuarioId_id == data["alunoId"])
+    treino_usuario.execute()
+    return jsonify({'message': 'TREINO REMOVIDO DO USUÁRIO'}), 200
+    
